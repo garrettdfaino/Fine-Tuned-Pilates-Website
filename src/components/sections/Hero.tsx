57226cf2e59ea3d09b24d25f693f5fb43d679838
garrettdfaino <garrettdf.su@gmail.com>
2025-05-11 15:34:18 -0400

    Update Hero.tsx
    hero change for pictures

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -33,7 +33,7 @@ export function Hero({ setShowContactModal }: HeroProps) {
           muted
           playsInline
           className="w-full h-full object-cover"
-          poster="https://github.com/garrettdfaino/Pictures-for-FTP/blob/main/inspection-2.jpg?raw=true"
+          poster="https://raw.githubusercontent.com/garrettdfaino/Pictures-for-FTP/main/inspection-2.jpg"
         >
           <source src="https://player.vimeo.com/external/434045526.sd.mp4?s=c27eecc69a27dbc4ff2b87d38afc35f1a9e7c02d&profile_id=164&oauth2_token_id=57447761" type="video/mp4" />
         </video>
